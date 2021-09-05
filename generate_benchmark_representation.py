95e8d23166428e46fe4f6d80b413ec3b7e5f719f
devaa <devaa@donnerluetjen.de>
2021-09-05 18:40:20 +0200

    rename file, rename methods, change path and filenames

--- generate_benchmark_representation.py
+++ generate_benchmark_representation.py
@@ -21,10 +21,6 @@ def transformFloat(value):
     return str(value).replace('.', ',')
 
 
-def generate_legend_dict(legend_array):
-    result = {}
-
-
 def ranking(scores, do_not_rank=[]):
     keys = scores.keys()
     squared_result = 0
@@ -40,7 +36,8 @@ def ranking(scores, do_not_rank=[]):
 
 
 def generate_average_diagram(json_path, score_name='ranking', do_not_rank=[]):
-    path_dict = path_dictionary(json_path, score_name)
+    path_dict = path_dictionary(json_path)
+    pgf_path = Path(path_dict['tex_dir'], f'{score_name}_pgfplots.tex')
     with open(json_path) as json_file:
         data = json.load(json_file)
 
@@ -50,40 +47,28 @@ def generate_average_diagram(json_path, score_name='ranking', do_not_rank=[]):
         metrics.remove('properties')
 
         header = ['metric', f'average-{score_name}', 'average-runtime']
-        pp.open_pgfplots_file(path_dict['tex_path'],
-                              f'Average {score_name.capitalize()} over Average Runtime',
-                              'Average Runtime', f'Average {score_name.capitalize()}')
-        pp.add_legend_to_pgfplots_file(path_dict['tex_path'], metrics)
+        pp.init_pgfplots_file(pgf_path,
+                              f'Mean {score_name.capitalize()} vs. Mean Runtime',
+                              'Mean Runtime', f'Mean {score_name.capitalize()}')
 
         for metric in metrics:
-            # replace filename with metric score_name
-            metric_csv_path = csv_path_for(metric, path_dict)
-            pp.addplot_line_to_pgfplots_file_for(path_dict['tex_path'],
-                                                 f'{metric}.csv',
-                                                 x_col=2, y_col=1)
+            # iterate over datasets for metric and find averages
+            accumulated_scoring = 0
+            accumulated_runtime = 0
+            for dataset in datasets:
+                if score_name == 'ranking':
+                    accumulated_scoring += ranking(data[dataset][metric], do_not_rank)
+                else:
+                    accumulated_scoring += data[dataset][metric][score_name]
+                accumulated_runtime += data[dataset][metric]['runtime']
 
-            with open(metric_csv_path, 'w') as csv_file:
-                csv_writer = csv.writer(csv_file)
-                csv_writer.writerow(header)
-
-                # iterate over datasets for metric and find averages
-                accumulated_scoring = 0
-                accumulated_runtime = 0
-                for dataset in datasets:
-                    if score_name == 'ranking':
-                        accumulated_scoring += ranking(data[dataset][metric], do_not_rank)
-                    else:
-                        accumulated_scoring += data[dataset][metric][score_name]
-                    accumulated_runtime += data[dataset][metric]['runtime']
-
-                csv_writer.writerow([metric,
-                                     accumulated_scoring / len(datasets),
-                                     accumulated_runtime / len(datasets)])
-        pp.close_pgfplots_file(path_dict['tex_path'])
+            table_data = [[accumulated_runtime / len(datasets), accumulated_scoring / len(datasets)]]
+            pp.add_table(pgf_path, metric, table_data)
 
 
 def generate_table(json_path, do_not_rank=[]):
-    path_dict = path_dictionary(json_path, 'score_table')
+    path_dict = path_dictionary(json_path)
+    table_path = Path(path_dict['tex_dir'], 'scores_table.tex')
     with open(json_path) as json_file:
         data = json.load(json_file)
 
@@ -98,7 +83,7 @@ def generate_table(json_path, do_not_rank=[]):
         for remove in do_not_rank:
             scores.remove(remove)
         
-        tt.open_score_table(path_dict['tex_table_path'], metrics, scores)
+        tt.open_score_table(table_path, metrics, scores)
         
         for dataset in datasets:
             dataset_data = [dataset]
@@ -110,21 +95,18 @@ def generate_table(json_path, do_not_rank=[]):
                         
                     score_value = data[dataset][metric][score]
                     dataset_data.append(score_value)
-            tt.add_table_line(path_dict['tex_table_path'], dataset_data)
+            tt.add_table_line(table_path, dataset_data)
             
         
-        tt.close_score_table(path_dict['tex_table_path'], 'UEA Datasets')
+        tt.close_score_table(table_path, 'UEA Datasets')
     
 
-def path_dictionary(json_path, score_name):
-    csv_converted_json_path = Path(json_path.replace('json', 'csv'))
-    csv_dir = Path(csv_converted_json_path.parent,
-                   csv_converted_json_path.stem, score_name)
-    csv_dir.mkdir(parents=True, exist_ok=True)
-    csv_path = Path(csv_dir, csv_converted_json_path.stem + '.csv')
-    tex_path = Path(csv_dir, f'pgfplots.tex')
-    tex_table_path = Path(csv_dir, f'scores_table.tex')
-    return {'csv_dir': csv_dir, 'csv_path': csv_path, 'tex_path': tex_path, 'tex_table_path': tex_table_path}
+def path_dictionary(json_path):
+    tex_converted_json_path = Path(json_path.replace('json', 'tex'))
+    tex_dir = Path(tex_converted_json_path.parent, tex_converted_json_path.stem)
+    tex_dir.mkdir(parents=True, exist_ok=True)
+    return {'tex_dir': tex_dir}
+
 
 def csv_path_for(metric, path_dict):
     return Path(path_dict['csv_dir'],
--- pgfplots.py
+++ pgfplots.py
@@ -9,108 +9,66 @@ __email__ = "s2092795@stud.uni-frankfurt.de"
 from pathlib import Path
 
 
-def open_pgfplots_file(tex_path, title="", x_label='x', y_label='y',
+def init_pgfplots_file(tex_path, title="", x_label='x', y_label='y',
                        data_path=None):
     tex_path = Path(tex_path)
     with open(tex_path, 'w') as pgf_file:
         pgf_file.write('\\begin{tikzpicture}\n')
-        pgf_file.write('    \\begin{axis}[\n')
-        pgf_file.write(f'        title = {title},\n')
-        pgf_file.write('        xmode = log,\n')
-        pgf_file.write(f'        xlabel = {{{x_label}}},\n')
-        pgf_file.write(f'        ylabel = {{{y_label}}},\n')
-        pgf_file.write('        grid = both,\n')
-        pgf_file.write(
-            '   	    grid style={line width=.2pt, draw=gray!10},\n')
-        pgf_file.write('        major grid style='
-                       '{line width=.2pt,draw=gray!50},\n')
-        pgf_file.write('        minor tick num=5,\n')
-        pgf_file.write('        legend style={\n')
-        pgf_file.write('		    font={\\small},\n')
-        pgf_file.write('        },\n')
-        pgf_file.write('        legend cell align={left},\n')
-        pgf_file.write('        legend pos = south east,\n')
-        pgf_file.write(
-            '        clip=false, % avoid clipping at edge of diagram\n')
-        pgf_file.write(
-            '        nodes near coords, % print the value near node\n')
-        pgf_file.write('    ]\n')
-
-
-def open_single_score_pgfplots_file(tex_path, title="", x_label='x',
-                                    y_label='y',
-                                    data_path=None):
-    tex_path = Path(tex_path)
-    with open(tex_path, 'w') as pgf_file:
-        pgf_file.write(
-            f'\\pgfplotstableread[col sep=comma] {{../'
-            f'{data_path}}}\datatable\n')
-        pgf_file.write('\\begin{tikzpicture}\n')
-        pgf_file.write('    \\begin{axis}[\n')
-        pgf_file.write(f'        title = {title},\n')
-        pgf_file.write(f'        xtick = data,\n')
-        pgf_file.write(
-            f'        xticklabels from table={{\\datatable}}{{dataset}},\n')
-        pgf_file.write(f'        xtick = data, %,\n')
-        pgf_file.write(
-            f'        x tick label style = {{rotate = 45, anchor = east}},\n')
-        pgf_file.write(f'        xlabel = {{{x_label}}},\n')
-        pgf_file.write(f'        ylabel = {{{y_label}}},\n')
-        pgf_file.write('        grid = both,\n')
-        pgf_file.write(
-            '   	    grid style={line width=.2pt, draw=gray!10},\n')
-        pgf_file.write('        major grid style='
-                       '{line width=.2pt,draw=gray!50},\n')
-        pgf_file.write('        minor tick num=5,\n')
-        pgf_file.write('        legend style={\n')
-        pgf_file.write('		    anchor = south,\n')
-        pgf_file.write(
-            '            at={(.52, .02)}, % x, y positions between 0 and 1 '
-            'of diagram extension,\n')
-        pgf_file.write('		    font={\\small},\n')
-        pgf_file.write('        },')
-        pgf_file.write('    ]\n')
-
-
-def close_pgfplots_file(tex_path):
-    with open(tex_path, 'a') as pgf_file:
-        pgf_file.write('    \\end{axis}\n')
+        pgf_file.write('\t\\begin{axis}[\n')
+        pgf_file.write('\t\ttable/col sep = comma,\n')
+        pgf_file.write('\t\txmode = log,\n')
+        pgf_file.write(f'\t\txlabel = {{{x_label}}},\n')
+        pgf_file.write(f'\t\tylabel = {{{y_label}}},\n')
+        pgf_file.write('\t\tgrid = both,\n')
+        pgf_file.write('\t\tgrid style={line width=.2pt, draw=gray!10},\n')
+        pgf_file.write('\t\tmajor grid style={line width=.2pt,draw=gray!50},\n')
+        pgf_file.write('\t\tminor tick num=5,\n')
+        pgf_file.write('\t\tlegend cell align={left},\n')
+        pgf_file.write('\t\tlegend pos = south east,\n')
+        pgf_file.write('\t\tclip=false, % avoid clipping at edge of diagram\n')
+        pgf_file.write('\t\tnodes near coords, % print the value near node\n')
+        pgf_file.write('\t]\n')
+        pgf_file.write('\t\\end{axis}\n')
         pgf_file.write('\\end{tikzpicture}\n')
 
 
-def addplot_line_to_pgfplots_file_for(tex_path, data_path, x_col=0, y_col=1):
+def add_table(tex_path, table_name, table_data=[[]]):
+    add_inline_table(tex_path, table_name, table_data)
+    add_inline_plot(tex_path, table_name)
+
+
+def add_inline_table(tex_path, table_name, table_data=[[]]):
+    # build inline table
+    inline_table = '\t\\pgfplotstableread[col sep=comma]{%\n'
+    for table_row in table_data:
+        inline_table += '\t\t' + ', '.join(map(str, table_row)) + '\n'
+    inline_table += f'\t}}\\{table_name}\n'
+    
+    add_inline_data(tex_path, 'table', inline_table)
+
+
+def add_inline_plot(tex_path, table_name, x_col=0, y_col=1):
     marks_only = '[only marks]'
     marks_only = ''
-    with open(tex_path, 'a') as pgf_file:
-        pgf_file.write(
-            f'        \\addplot+ {marks_only}'
-            f' table['
-            f' col sep = comma,'
-            f' x index = {{{x_col}}},'
-            f' y index = {{{y_col}}}'
-            f']'
-            f'{{{data_path}}};\n')
+    inline_plot = f'\t\t\\addplot+ {marks_only} table[ x index = {{{x_col}}}, y index = {{{y_col}}}]{{\\{table_name}}};\n'
+    inline_plot += f'\t\t\\addlegendentry{{{table_name}}}\n'
+    add_inline_data(tex_path, 'plot', inline_plot)
+    
+
+def add_inline_data(tex_path, insertion_key, data):
+    with open(tex_path, 'r') as pgf_file:
+        pgf_lines = pgf_file.readlines()
+    
+    insertion_index = find_insertion_index_for(insertion_key, pgf_lines)
+    pgf_lines.insert(insertion_index, data)
+    pgf_contents = ''.join(pgf_lines)
+    
+    with open(tex_path, 'w') as pgf_file:
+        pgf_file.write(pgf_contents)
 
 
-def addplot_line_to_single_score_pgfplots_file_for(tex_path, data_path,
-                                                   x_col=0, y_col=1):
-    marks_only = '[only marks]'
-    marks_only = ''
-    with open(tex_path, 'a') as pgf_file:
-        pgf_file.write(
-            f'        \\addplot+ {marks_only}'
-            f' table['
-            f' col sep = comma,'
-            f' x expr=\\coordindex,'
-            f' y index = {{{y_col}}}'
-            f']'
-            f'{{{data_path}}};\n')
-
-
-def add_legend_to_pgfplots_file(tex_path, legend):
-    with open(tex_path, 'a') as pgf_file:
-        pgf_file.write('        \legend{\n')
-        for entry in legend:
-            entry = entry.replace("_manhattan", "")
-            pgf_file.write(f'           {entry},\n')
-        pgf_file.write('        }\n')
+def find_insertion_index_for(key, string_list):
+    insertion_points_to_find = {'table': 'begin{axis}[', 'plot': 'end{axis}'}
+    insertion_list = [s for s in string_list if insertion_points_to_find[key] in s]
+    insertion_index = string_list.index(insertion_list[0])
+    return string_list.index(insertion_list[0])
