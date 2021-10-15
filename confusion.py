56451a9e91112fa8db51c4acfb7e7e4ecbd56a16
devaa <devaa@donnerluetjen.de>
2021-10-15 10:24:10 +0200

    store all derived values

--- confusion.py
+++ confusion.py
@@ -29,20 +29,26 @@ def add_confusion_values(input_json, output_json):
                 total = dataset_details[dataset]['num_of_test_instances']
 
                 # derive confusion values
-                precision = (f1 * recall) / (2 * recall - f1)
-                tp = total * (1 - auroc) / (1 / precision + 1 / recall - 2)
+                derived_precision = (f1 * recall) / (2 * recall - f1)
+                tp = total * (1 - auroc) / (1 / derived_precision + 1 / recall - 2)
                 tn = accuracy * total - tp
-                fp = tp / precision - tp
+                fp = tp / derived_precision - tp
                 fn = tp / recall - tp
                 specificity = tn / (fp + tn)
                 tpr = recall
                 fpr = 1 - specificity
-                new_auroc = (fpr * tpr / 2) + (specificity * (1 - tpr) / 2) + (specificity * tpr)
+                derived_accuracy = (tp + tn) / (tp + fp + tn + fn)
+                derived_recall = tp / (tp + fn)
+                derived_f1_score = (2 * derived_precision * derived_recall) / (derived_precision + derived_recall)
+                derived_auroc = (fpr * tpr / 2) + (specificity * (1 - tpr) / 2) + (specificity * tpr)
 
                 # store in data
-                data[dataset][metric]['precision'] = precision
+                data[dataset][metric]['precision'] = derived_precision
                 data[dataset][metric]['specificity'] = specificity
-                data[dataset][metric]['new-auroc'] = new_auroc
+                data[dataset][metric]['derived-accuracy'] = derived_accuracy
+                data[dataset][metric]['derived-recall'] = derived_recall
+                data[dataset][metric]['derived-f1-score'] = derived_f1_score
+                data[dataset][metric]['derived-auroc'] = derived_auroc
                 data[dataset][metric]['tp'] = tp
                 data[dataset][metric]['tn'] = tn
                 data[dataset][metric]['fp'] = fp
@@ -56,7 +62,7 @@ def add_confusion_values(input_json, output_json):
 if __name__ == '__main__':
     json_store = './Benchmarks/json/'
     json_raw_file = 'UEA_archive_wws--1.json'
-    json_confusion_added_file = 'UEA_archive_wws--1_plus_confusion.json'
+    json_confusion_added_file = 'UEA_archive_wws--1_plus_derived.json'
 
     add_confusion_values(json_store + json_raw_file,
                          json_store + json_confusion_added_file)
